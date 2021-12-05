8f7a5696afa5a96077e4027fb02b6c5c2939339a
ronaldrezal <39642740+ronaldrezal@users.noreply.github.com>
2021-12-05 16:49:48 +0800

    bismillah
    semoga berkembang

---
+++ bahasa.h
@@ -0,0 +1,9 @@
+#define jika if
+#define tunda delay
+#define hidupkanled digitalWrite(LED_BUILTIN,HIGH)
+#define matikanled digitalWrite(LED_BUILTIN,LOW)
+#define fungsikanpin pinMode
+#define sebagaioutput OUTPUT
+#define ledbawaan LED_BUILTIN
+#define programAwal void setup() 
+#define programBerulang void loop()
